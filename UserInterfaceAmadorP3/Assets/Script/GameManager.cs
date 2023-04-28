edff4a096788238e49359a2ffd69e6d8b42f2cbc
TheFALCON736 <angel7amador06@gmail.com>
2023-04-28 11:42:55 -0700

    d
    d

--- UserInterfaceAmadorP3/Assets/Script/GameManager.cs
+++ UserInterfaceAmadorP3/Assets/Script/GameManager.cs
@@ -13,6 +13,8 @@ public class GameManager : MonoBehaviour
     public Button restartButton;
     public GameObject titleScreen;
     public bool isGameActive;
+    public TextMeshProUGUI livesText;
+    private int lives;
     private int score;
     private float spawnRate = 1.0f;
 
@@ -64,7 +66,19 @@ public class GameManager : MonoBehaviour
 
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
+        UpdateLives(3);
 
         titleScreen.gameObject.SetActive(false);
+
+    }
+
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
     }
 }
